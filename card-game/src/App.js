3bb06de5ce71c457a96130f91254e7f72c6892d5
PeerapatThangsuksan <6010110245@psu.ac.th>
2019-09-21 22:52:15 +0700

    checkpoint 5
    checkpoint 5 -

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import'./App.css';
+import CharacterCard from './CharacterCard'
+import WordCard from './WordCard'
+const word = "Hello";
+class App extends Component{
+  render(){
+    return(
+      <div className="APP">
+        {
+        <WordCard value="hello"/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        }
+      </div>
+    );
+  }
+}export default App;
