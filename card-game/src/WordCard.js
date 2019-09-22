6d80732f14a9d69eb0434bf7984c775fd14d8b4e
PeerapatThangsuksan <6010110245@psu.ac.th>
2019-09-22 12:04:45 +0700

    Check Results
    Check Results

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -36,7 +36,9 @@ export default class WordCard extends Component{
     render(){
         return (
             <div>
-                { Array.from(this.state.chars).map((c,i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>)}
+                <h2> This Round : {this.state.attempt} </h2>
+                { Array.from(this.state.chars).map((c,i) => <CharacterCard value={c} key={i} attempt ={this.state.attempt} activationHandler={this.activationHandler}/>)}
+                
             </div>
         );
     }
